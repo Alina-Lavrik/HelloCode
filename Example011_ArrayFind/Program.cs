65cf43efb2a6202d81ca86dc34e810717ab350fc
Alina Lavrik <alina_voro@mail.ru>
2022-09-18 21:40:43 +0300

    Example 011 Array_Find

---
+++ Example011_ArrayFind/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;  //возвращает длину или количество элементов массива;
+int find = 18;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;   // остановит поиск после нахождения первого нужного номера индекса 
+    }
+    
+    //index = index + 1;
+    index++;
+}
